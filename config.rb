3bd83e41cc92506e481324a89f6cc1e734ae143a
Bob Waycott <bobwaycott@gmail.com>
2013-08-14 22:38:27 -0400

    post-setup rakefile & config

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/cfaa/"
+http_images_path = "/cfaa/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/cfaa/fonts"
+css_dir = "public/cfaa/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
